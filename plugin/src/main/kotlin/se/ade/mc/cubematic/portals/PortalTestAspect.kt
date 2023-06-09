f77f06df8ca6c71f6154cabf2c0bddb13889142d
ade <ade@ade.se>
2023-06-10 00:30:30 +0200

    Portals: changed implementation to use frames of crying obsidian, and activate by right clicking with a lodestone compass with curse of vanishing

--- plugin/src/main/kotlin/se/ade/mc/cubematic/portals/PortalTestAspect.kt
+++ plugin/src/main/kotlin/se/ade/mc/cubematic/portals/PortalTestAspect.kt
@@ -13,15 +13,18 @@ import org.bukkit.block.Container
 import org.bukkit.block.EndGateway
 import org.bukkit.block.data.Directional
 import org.bukkit.block.data.Orientable
+import org.bukkit.enchantments.Enchantment
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
+import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.block.BlockDispenseEvent
 import org.bukkit.event.entity.EntityPortalEnterEvent
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.event.player.PlayerPortalEvent
 import org.bukkit.event.player.PlayerTeleportEvent
+import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.CompassMeta
 import se.ade.mc.cubematic.ForkCompat
 import se.ade.mc.cubematic.extensions.Aspect
@@ -52,10 +55,14 @@ private val flatNeighbors = arrayOf(
 
 private const val ALLOW_NON_PLAYER_ENTITIES = false
 private const val IMMEDIATE_MODE = true
-private val PORTAL_MATERIAL = Material.END_GATEWAY
+private const val MAX_PORTAL_SIZE = 21
+
+private val portalMaterial = Material.END_GATEWAY
+private val frameMaterial = Material.CRYING_OBSIDIAN
 
 class PortalTestAspect(private val cubematic: CubematicPlugin): Listener, Aspect(cubematic) {
     private val debug = true
+    val logger: Logger? = if(debug) cubematic.logger else null
 
     init {
         ForkCompat.handleEntityEndGatewayTeleport(this) { entityTeleportEvent, endGateway ->
@@ -66,7 +73,7 @@ class PortalTestAspect(private val cubematic: CubematicPlugin): Listener, Aspect
 
     @EventHandler
     fun onEvent(e: EntityPortalEnterEvent) {
-        if(IMMEDIATE_MODE && e.entity is Player && e.location.block.type == PORTAL_MATERIAL) {
+        if(IMMEDIATE_MODE && e.entity is Player && e.location.block.type == portalMaterial) {
             onPortalActivated(e.location, e.entity as Player)
         }
     }
@@ -92,11 +99,11 @@ class PortalTestAspect(private val cubematic: CubematicPlugin): Listener, Aspect
      */
 
     private fun onPortalActivated(location: Location, player: Player): Boolean {
-        val portalStart = if(location.block.type == PORTAL_MATERIAL) {
+        val portalStart = if(location.block.type == portalMaterial) {
             location.block
         } else {
             location.block.getNeighborsCubic(1)
-                .filter { it.type == PORTAL_MATERIAL }
+                .filter { it.type == portalMaterial }
                 .map { it.location.getCenter() }
                 .nearestOrNull(location)
                 ?.block
@@ -105,7 +112,7 @@ class PortalTestAspect(private val cubematic: CubematicPlugin): Listener, Aspect
         if(portalStart == null)
             return false
 
-        val portalBlocks = portalStart.findAllChaining(PORTAL_MATERIAL)
+        val portalBlocks = portalStart.findAllChaining(portalMaterial)
 
         val compass = findPortalCompass(portalBlocks)
             ?: return false
@@ -170,13 +177,53 @@ class PortalTestAspect(private val cubematic: CubematicPlugin): Listener, Aspect
         return null
     }
 
+    @EventHandler
+    fun onEvent(event: BlockBreakEvent) {
+        if(event.block.type != Material.CRYING_OBSIDIAN)
+            return
+
+        val gateways = adjacentFaces.mapNotNull {
+            val block = event.block.getRelative(it)
+            if(block.type == Material.END_GATEWAY)
+                block
+            else
+                null
+        }
+
+        if(gateways.isEmpty())
+            return
+
+        //Todo: Find disjoint groups of gateway blocks
+        val gwStartBlock = gateways.first()
+        val target = (gwStartBlock.state as EndGateway).exitLocation?.clone()?.add(0.0, -1.0, 0.0)
+
+        logger?.info("Target: $target")
+
+        val allBlocks = gwStartBlock.findAllChaining(Material.END_GATEWAY)
+        allBlocks.forEach {
+            it.type = Material.AIR
+        }
+
+        if(target != null) {
+            val compass = ItemStack(Material.COMPASS)
+
+            compass.itemMeta = (compass.itemMeta as CompassMeta).also {
+                it.lodestone = target.clone()
+                it.isLodestoneTracked = true
+                it.addEnchant(Enchantment.VANISHING_CURSE, 1, true)
+            }
+
+            logger?.info(compass.itemMeta.toString())
+            gwStartBlock.world.dropItem(gwStartBlock.location, compass)
+        }
+    }
 
     @EventHandler
     fun onEvent(event: PlayerInteractEvent) {
         if (event.action != Action.RIGHT_CLICK_BLOCK)
             return
 
-        val item = event.item ?: return
+        val item = event.player.inventory.itemInMainHand ?: return
         val block = event.clickedBlock ?: return
 
         if(item.type == Material.COMPASS && block.type == Material.LODESTONE) {
@@ -184,94 +231,68 @@ class PortalTestAspect(private val cubematic: CubematicPlugin): Listener, Aspect
                 cubematic.server.broadcastMessage(item.itemMeta.toString())
             }
         }
+
+        if(item.type == Material.COMPASS && block.type == frameMaterial) {
+            val compass = item.itemMeta as CompassMeta
+            if(!compass.isLodestoneTracked)
+                return
+
+            if(!item.enchantments.containsKey(Enchantment.VANISHING_CURSE))
+                return
+
+            val activationBlock = block.getRelative(event.blockFace)
+            event.isCancelled = true
+            if(activateFrame(activationBlock, compass)) {
+                event.player.inventory.setItemInMainHand(null)
+            }
+        }
     }
 
-    @EventHandler
-    fun onEvent(event: BlockDispenseEvent) {
+    private fun activateFrame(block: Block, compass: CompassMeta): Boolean {
         val logger: Logger? = if(debug) cubematic.logger else null
 
-        if (event.block.type != Material.DISPENSER || event.item.type != Material.COMPASS)
-            return
+        val portalBlocks = findValidPortalBlocks(block)
+            ?: return false
 
-        val compass = event.item.itemMeta as CompassMeta
-        if(!compass.isLodestoneTracked)
-            return
+        if(portalBlocks.blocks.isEmpty())
+            return false
 
-        event.isCancelled = true
-
-        val dispenser = event.block.toDispenser()
-        val dispenserFacing = dispenser.facingBlock()
-
-        if(dispenserFacing.type != Material.DROPPER && dispenserFacing.type != Material.DISPENSER) {
-            return
-        }
-
-        val fuelContainer = dispenserFacing.state as? Container
-            ?: return
-
-        val targetBlock = fuelContainer.block.facingBlock()
-            ?: return
-
-        if(targetBlock.type == PORTAL_MATERIAL)
-            return
-
-        if(targetBlock.type != Material.AIR) {
-            logger?.info("Target block not AIR")
-            return
-        }
-
-        val portalBlocks = findValidPortalBlocks(fuelContainer.block)
-            ?: return
-
-        val fruitIndex = fuelContainer.inventory.indexOfFirst { it.type == Material.POPPED_CHORUS_FRUIT }
-
-        if(fruitIndex == -1) {
-            return
-        }
-
-        val fuelStack = fuelContainer.inventory.getItem(fruitIndex)
-            ?: return
-
-        val newFuelStack = when(val amount = fuelStack.amount) {
-            1 -> null
-            else -> fuelStack.clone().also { it.amount = amount - 1 }
-        }
-
-        fuelContainer.inventory.setItem(fruitIndex, newFuelStack)
         portalBlocks.blocks.forEach {
-            it.type = PORTAL_MATERIAL
-            //it.state.update(true, false)
-            if(PORTAL_MATERIAL == Material.NETHER_PORTAL) {
-                it.blockData = (it.blockData as Orientable).also { it.axis = portalBlocks.axis }
-            }
+            it.type = portalMaterial
 
-            if(PORTAL_MATERIAL == Material.END_GATEWAY) {
-                (it.state as EndGateway).also {
-                    it.isExactTeleport = true
-                    it.exitLocation = compass.lodestone?.clone()?.add(0.5, 1.0, 0.5)
-                    it.age = Long.MIN_VALUE
-                    it.update()
+            if(portalMaterial == Material.END_GATEWAY) {
+                (it.state as EndGateway).also { gw ->
+                    gw.isExactTeleport = true
+                    gw.exitLocation = compass.lodestone?.clone()?.add(0.0, 1.0, 0.0)
+                    gw.age = Long.MIN_VALUE
+                    gw.update()
                 }
             }
         }
 
+        return true
     }
 
-    private fun findValidPortalBlocks(portalPlacer: Block): PortalBlocks? {
-        val frameType = Material.GOLD_BLOCK
-        val direction = (portalPlacer.blockData as? Directional)?.facing
+    private fun findValidPortalBlocks(start: Block): PortalBlocks? {
+        val logger: Logger? = if(debug) cubematic.logger else null
+
+        val frameType = frameMaterial
+
+        val frameTypes = listOf(frameType)
+        val up = start.findFirst(frameTypes, BlockFace.UP, MAX_PORTAL_SIZE)
             ?: return null
 
-        val frameTypes = listOf(frameType, Material.DISPENSER, Material.DROPPER)
-        val end = portalPlacer.findFirst(frameTypes, direction, 21)
+        val down = start.findFirst(frameTypes, BlockFace.DOWN, MAX_PORTAL_SIZE)
             ?: return null
 
-        val startColumn = portalPlacer.column(end)
+        val startColumn = down.column(up)
             ?: return null
 
         val startColumnContent = startColumn.drop(1).dropLast(1)
-        if(startColumnContent.any { it.type != Material.AIR })
+        if(startColumnContent.isEmpty() || startColumnContent.any { it.type != Material.AIR }) {
+            logger?.info("Column is empty or invalid")
             return null
+        }
 
         val north = expandPortal(startColumn, BlockFace.NORTH, frameTypes)
         val south = expandPortal(startColumn, BlockFace.SOUTH, frameTypes)
@@ -351,10 +372,10 @@ fun Block.findAllChaining(type: Material): Set<Block> {
     do {
         val newBlocks = todo.flatMap { current ->
             if (checked.contains(current)) return@flatMap listOf<Block>()
+            checked.add(current)
 
             adjacentFaces.mapNotNull { face ->
                 val candidate = current.getRelative(face)
-                checked.add(candidate)
 
                 when (candidate.type) {
                     type -> candidate
@@ -386,7 +407,9 @@ fun Block.column(that: Block): List<Block>? {
     if(that.x != this.x || that.z != this.z)
         return null
 
-    return (this.y..that.y).map { this.world.getBlockAt(this.x, it, this.z) }
+    return (this.y..that.y).map {
+        this.world.getBlockAt(this.x, it, this.z)
+    }
 }
 
 data class PortalBlocks(val axis: Axis, val blocks: List<Block>)
