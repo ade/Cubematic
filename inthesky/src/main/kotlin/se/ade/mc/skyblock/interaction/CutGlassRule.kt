d0ec580270c58175d5477618f7f474a889a1d8f8
ade <ade@ade.se>
2025-05-22 22:50:33 +0200

    Cut glass / bottle to sand rule

---
+++ inthesky/src/main/kotlin/se/ade/mc/skyblock/interaction/CutGlassRule.kt
@@ -0,0 +1,46 @@
+package se.ade.mc.skyblock.interaction
+
+import org.bukkit.Material
+import org.bukkit.NamespacedKey
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.RecipeChoice
+import org.bukkit.inventory.StonecuttingRecipe
+import se.ade.mc.skyblock.CubematicSkyPlugin
+
+fun cutGlassRule(plugin: CubematicSkyPlugin) {
+	plugin.server.addRecipe(
+		StonecuttingRecipe(
+			/*key = */ NamespacedKey(plugin, "stonecutter_glass_bottle"),
+			/*result = */ ItemStack(Material.SAND),
+			/*source= */ Material.GLASS_BOTTLE,
+		)
+	)
+	plugin.server.addRecipe(
+		StonecuttingRecipe(
+			/*key = */ NamespacedKey(plugin, "stonecutter_glass_block"),
+			/*result = */ ItemStack(Material.SAND),
+			/* input = */
+			RecipeChoice.MaterialChoice(
+				listOf(
+					Material.GLASS,
+					Material.BLACK_STAINED_GLASS,
+					Material.BLUE_STAINED_GLASS,
+					Material.BROWN_STAINED_GLASS,
+					Material.CYAN_STAINED_GLASS,
+					Material.GRAY_STAINED_GLASS,
+					Material.GREEN_STAINED_GLASS,
+					Material.LIGHT_BLUE_STAINED_GLASS,
+					Material.LIGHT_GRAY_STAINED_GLASS,
+					Material.LIME_STAINED_GLASS,
+					Material.MAGENTA_STAINED_GLASS,
+					Material.ORANGE_STAINED_GLASS,
+					Material.PINK_STAINED_GLASS,
+					Material.PURPLE_STAINED_GLASS,
+					Material.RED_STAINED_GLASS,
+					Material.WHITE_STAINED_GLASS,
+					Material.YELLOW_STAINED_GLASS,
+				)
+			),
+		)
+	)
+}
--- inthesky/src/main/kotlin/se/ade/mc/skyblock/interaction/InteractionFacet.kt
+++ inthesky/src/main/kotlin/se/ade/mc/skyblock/interaction/InteractionFacet.kt
@@ -1,22 +1,18 @@
 package se.ade.mc.skyblock.interaction
 
-import org.bukkit.Material
-import org.bukkit.damage.DamageType
-import org.bukkit.entity.EntityType
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockBurnEvent
 import org.bukkit.event.entity.EntityDeathEvent
 import org.bukkit.event.player.PlayerInteractEvent
-import org.bukkit.inventory.ItemStack
 import se.ade.mc.cubematic.extensions.Aspect
 import se.ade.mc.skyblock.CubematicSkyPlugin
-import kotlin.random.Random
 
 class InteractionFacet(val plugin: CubematicSkyPlugin): Aspect(plugin) {
 	override fun enable() {
 		plugin.server.pluginManager.registerEvents(listener, plugin)
 		charcoalOnCampfireRecipe(plugin)
+		cutGlassRule(plugin)
 	}
 
 	override fun disable() {
