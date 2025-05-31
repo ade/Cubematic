b2abd4cb8d44a68c682318dce96919d7033685a5
ade <ade@ade.se>
2025-05-31 20:46:15 +0200

    minor

--- inthesky/src/main/kotlin/se/ade/mc/skyblock/generator/overworld/HardOverworldGenerator.kt
+++ inthesky/src/main/kotlin/se/ade/mc/skyblock/generator/overworld/HardOverworldGenerator.kt
@@ -3,7 +3,9 @@ package se.ade.mc.skyblock.generator.overworld
 import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.World
+import org.bukkit.generator.BlockPopulator
 import org.bukkit.generator.ChunkGenerator
+import org.bukkit.generator.LimitedRegion
 import org.bukkit.generator.WorldInfo
 import se.ade.mc.skyblock.CubematicSkyPlugin
 import java.util.Random
@@ -34,12 +36,28 @@ class HardOverworldGenerator(private val plugin: CubematicSkyPlugin) : ChunkGene
 		return Location(world, 8.0, 64.0, 8.0)
 	}
 
+	override fun getDefaultPopulators(world: World): List<BlockPopulator?> {
+		return listOf(Populator)
+	}
+
 	private fun createStartIsland(chunkData: ChunkData) {
 		// 7x7 island of dirt
 		for (x in 4..10) {
 			for (z in 4..10) {
 				chunkData.setBlock(x, 64, z, Material.DIRT)
 			}
+		}
+	}
+
+	private object Populator : BlockPopulator() {
+		override fun populate(
+			worldInfo: WorldInfo,
+			random: Random,
+			chunkX: Int,
+			chunkZ: Int,
+			limitedRegion: LimitedRegion
+		) {
+			//TODO
 		}
 	}
 }
